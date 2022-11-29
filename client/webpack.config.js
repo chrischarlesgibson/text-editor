2cc3df69ffb16bff6213f7c89aa9322ab4129ae1
Chris Gibson <chrischarlesgibson@gmail.com>
2022-11-28 22:19:43 -0600

    troubleshooting routes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "text editor",
+        title: "JATE",
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
@@ -29,7 +29,7 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "text editor",
+        name: "text-editor",
         short_name: "text",
         description: "enter your text notes",
         background_color: "#272822",
